733eaed1b42db85a4adcf3e7ce090fbe255ed471
Konstantinos Iliakis <iliakiskostis@gmail.com>
2016-05-11 20:49:45 +0200

    almost working

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -3,6 +3,7 @@
 
 #include "utils.h"
 #include <thrust/host_vector.h>
+#include "stdio.h"
 
 /* Red Eye Removal
    ===============
@@ -42,8 +43,8 @@
 
  */
 
-__global__ void simple_scan(const size_t * const input,
-                            size_t * const output,
+__global__ void simple_scan(const unsigned int * const input,
+                            unsigned int * const output,
                             const size_t numElems)
 {
 
@@ -55,9 +56,9 @@ __global__ void simple_scan(const size_t * const input,
 
    data[id] = input[id];
    
-   for (size_t s = 1; s < blockDim.x; s*=2)
+   for (unsigned int s = 1; s < blockDim.x; s*=2)
    {
-      __synchthreads();
+      __syncthreads();
       if(id >= s){
          data[id] += data[id - s];
       }
@@ -68,16 +69,16 @@ __global__ void simple_scan(const size_t * const input,
 }
 
 __global__ void apply_predicate(const unsigned int * const input, 
-                                 size_t * const predicate,
+                                 unsigned int * const predicate,
                                  const size_t numElems,
                                  const unsigned int sb)
 {
    size_t gid = blockIdx.x * blockDim.x + threadIdx.x;
-   size_t id = threadIdx.x;
+   //size_t id = threadIdx.x;
 
-   if(id > numElems) return;
+   if(gid > numElems) return;
 
-   predicate[id] = (input[id] & sb) == 0;
+   predicate[gid] = (input[gid] & sb) == 0;
 
 }
 
@@ -86,23 +87,27 @@ __global__ void change_positions(const unsigned int* const inputVals,
                                  const unsigned int* const inputPos,
                                  unsigned int* const outputVals,
                                  unsigned int* const outputPos,
-                                 const size_t* const scan0,
-                                 const size_t* const scan1,
-                                 const size_t* const predicate,
+                                 const unsigned int* const scan0,
+                                 const unsigned int* const scan1,
+                                 const unsigned int* const predicate,
                                  const size_t numElems,
-                                 const size_t* sum)
+                                 const unsigned int* sum)
 {
    size_t gid = blockIdx.x * blockDim.x + threadIdx.x;
-   size_t id = threadIdx.x;
-   size_t offset = sum[0];
-   if(id > numElems) return;
+   //size_t id = threadIdx.x;
+   //size_t offset = sum[0];
+   size_t offset = 0;
+  
+   if(gid > numElems) return;
 
-   if(predicate[id]){
-      outputPos[scan0[id]] = inputPos[id]; 
-      outputVals[scan0[id]] = inputVals[id]; 
+   if(predicate[gid] == 1){
+      outputPos[scan0[gid]] = inputPos[gid]; 
+      outputVals[scan0[gid]] = inputVals[gid]; 
    }else{
-      outputPos[scan1[id] + offset] = inputPos[id]; 
-      outputVals[scan1[id] + offset] = inputVals[id];       
+      //if(scan1[gid] + offset > numElems)
+      //   printf("This is bad\n");
+      outputPos[scan1[gid] + offset] = inputPos[gid]; 
+      outputVals[scan1[gid] + offset] = inputVals[gid];       
    }
 
 }
@@ -115,14 +120,14 @@ __global__ void reduce_sum(const unsigned int* const input,
    size_t gid = blockIdx.x * 2 * blockDim.x + threadIdx.x;
    size_t id = threadIdx.x;
 
-   if(id > numElems) return;
+   if(gid +blockDim.x > numElems) return;
    __shared__ extern unsigned int data[];
 
-   data[id] = input[id] + input[id + blockDim.x];
+   data[id] = input[gid] + input[gid + blockDim.x];
 
-   for (size_t s = blockDim.x/2; s > 0; s>>=1){
-      __synchthreads();
-      size_t other = id + s;
+   for (unsigned int s = blockDim.x/2; s > 0; s>>=1){
+      __syncthreads();
+      unsigned int other = id + s;
       if(id < s && other < numElems){
          data[id] += data[other];
       }
@@ -135,32 +140,43 @@ __global__ void reduce_sum(const unsigned int* const input,
 
 }
 
-void your_sort(unsigned int* const d_inputVals,
-               unsigned int* const d_inputPos,
-               unsigned int* const d_outputVals,
-               unsigned int* const d_outputPos,
+
+
+__global__ void flip_array(unsigned int* const input, 
+                           const size_t numElems)
+{
+   size_t gid = blockIdx.x * 2 * blockDim.x + threadIdx.x;
+   //size_t id = threadIdx.x;
+   if(gid > numElems) return;
+   input[gid] = !input[gid];
+}
+
+void your_sort(unsigned int*  d_inputVals,
+               unsigned int*  d_inputPos,
+               unsigned int*  d_outputVals,
+               unsigned int*  d_outputPos,
                const size_t numElems)
 { 
   
    size_t threads = 1024;
    size_t blocks = (numElems + threads -1) / threads;
 
-   size_t * d_predicate;
-   size_t * d_scan0;
-   size_t * d_scan1;
-   size_t * d_sum1;
-   size_t * d_sum2;
+   unsigned int * d_predicate;
+   unsigned int * d_scan0;
+   unsigned int * d_scan1;
+   unsigned int * d_sum1;
+   unsigned int * d_sum2;
 
-   checkCudaErrors(cudaMalloc(d_predicate,  sizeof(size_t) * numElems));
-   checkCudaErrors(cudaMalloc(d_scan0,  sizeof(size_t) * numElems));
-   checkCudaErrors(cudaMalloc(d_scan1,  sizeof(size_t) * numElems));
-   checkCudaErrors(cudaMalloc(d_sum1,  sizeof(size_t) * ((numElems +threads-1)/threads)));
-   checkCudaErrors(cudaMalloc(d_sum2,  sizeof(size_t) * ((numElems +threads-1)/threads)));
+   checkCudaErrors(cudaMalloc((void **) &d_predicate,  sizeof(unsigned int) * numElems));
+   checkCudaErrors(cudaMalloc((void **) &d_scan0,  sizeof(unsigned int) * numElems));
+   checkCudaErrors(cudaMalloc((void **) &d_scan1,  sizeof(unsigned int) * numElems));
+   checkCudaErrors(cudaMalloc((void **) &d_sum1,  sizeof(unsigned int) * numElems));
+   checkCudaErrors(cudaMalloc((void **) &d_sum2,  sizeof(unsigned int) * numElems));
 
    unsigned int sb = 1<<0;   
    size_t memoryBytes = threads * sizeof(unsigned int);
    //for (size_t i = 0; i < 32; ++i){
-   while(sb < UINT_MAX){
+   while(sb < (1<<31)){
       apply_predicate<<<blocks, threads>>>(d_inputVals, d_predicate, 
                                           numElems, sb);
       cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
@@ -171,10 +187,10 @@ void your_sort(unsigned int* const d_inputVals,
      
       // This should be working
       size_t elems = numElems;
-      checkCudaErrors(cudaMemcpy(d_predicate, d_sum1, sizeof(size_t) * numElems, cudaMemcpyDeviceToDevice));
+      checkCudaErrors(cudaMemcpy(d_predicate, d_sum1, sizeof(unsigned int) * numElems, cudaMemcpyDeviceToDevice));
       do{
          size_t numBlocks = (elems + threads-1)/threads;
-         size_t bytes = threads * sizeof(size_t);
+         size_t bytes = threads * sizeof(unsigned int);
          reduce_sum<<<numBlocks, threads, bytes>>>(d_sum1, d_sum2, elems);
          elems = numBlocks;
          std::swap(d_sum1, d_sum2);
@@ -191,12 +207,17 @@ void your_sort(unsigned int* const d_inputVals,
 
       change_positions<<<blocks, threads>>>(d_inputVals, d_inputPos,
                                             d_outputVals, d_outputPos,
-                                            d_scan0, d_scan1, numElems, d_sum2);
+                                            d_scan0, d_scan1, d_predicate, 
+                                            numElems, d_sum2);
       cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
       std::swap(d_inputPos, d_outputPos);
       std::swap(d_inputVals, d_outputVals);
       sb = sb << 1;
+      std::cout << "end of iteration no " << sb <<'\n';
    }
+   //std::swap(d_inputPos, d_outputPos);
+   //   std::swap(d_inputVals, d_outputVals);
+   std::cout << "its over\n";
 
 }
